033e8a434cb02df7fb288fa0e3c8855cb97bd61d
Ren Odion <ren.odion@duke.edu>
2020-06-04 06:20:25 -0400

    Added emacs dotfile

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,72 @@
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+
+(eval-when-compile
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+(use-package which-key
+  :config (which-key-mode)) ;; global which key mode
+
+(use-package all-the-icons)
+
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-horizon t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  ;; (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-horizon") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+  
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+;; Recent files
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; Basic config stuff
+;; User customizations
+(setq-default case-fold-search nil) ;; No search case
+(setq scroll-preserve-screen-position `t)
+(setq user-mail-address "smakey18@gmail.com") ;; Default email
+(tool-bar-mode -1)                             ; No toolbar
+(scroll-bar-mode -1)                           ; No scrollbar
+(menu-bar-mode -1)                             ; No menubar
+(setq inhibit-startup-message t)               ; No message at startup
+(setq visible-bell t)                          ; No beep when reporting errors
+(global-hl-line-mode t)                        ; Highlight cursor line
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized))))) ;; Start fullscreen
+(setq-default indent-tabs-mode nil)            ; Use spaces instead of tabs
+(show-paren-mode 1)                            ; Highlight parenthesis pairs
+(setq blink-matching-paren-distance nil)       ; Blinking parenthesis
+(setq show-paren-style 'expression)            ; Highlight text between parens
