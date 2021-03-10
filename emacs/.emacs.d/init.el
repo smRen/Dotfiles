8e8b52ce90110f0d2b3f45e1746fadb0954ed865
Ren Odion <smakey18@gmail.com>
2021-03-10 03:59:22 +0000

    Readd emacs configs and remove app folder

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,9 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
