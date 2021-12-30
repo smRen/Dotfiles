94e9e31dae6014f1b7df20c3113abc103aa47db8
Ren Odion <smakey18@gmail.com>
2021-12-30 14:52:42 -0500

    Move custom config to other file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -11,20 +11,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(dolist (filename '("packages.el" "user_settings.el" "custom.el"))
+(dolist (filename '("packages.el" "user_settings.el"))
   (load-file (expand-file-name filename user-emacs-directory)))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(lsp-ivy all-the-icons-ivy-rich ivy-rich ivy-prescient counsel-projectile counsel writeroom-mode which-key web-mode vterm use-package undo-fu tree-sitter-langs smartparens sly projectile magit lsp-ui lsp-pyright json-mode format-all flycheck evil-terminal-cursor-changer evil-surround evil-matchit evil-commentary evil-collection emmet-mode doom-themes doom-modeline company cmake-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
