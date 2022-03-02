2240e87b6b143f13e2fd54c3e958e88c380ebe67
Ren Odion <smakey18@gmail.com>
2022-03-02 00:48:57 -0500

    Fix bash script

--- scripts/Scripts/startup.sh
+++ scripts/Scripts/startup.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $HOSTNAME = "kubuntu" ]; then
+if [ "$HOSTNAME" = kubuntu ]; then
     /usr/bin/setxkbmap -option 'caps:ctrl_modifier' && xcape -e 'Caps_Lock=Escape' -t 100
-    /usr/bin/imwheel -b "4 5"
+    /usr/bin/imwheel -b '4 5'
     /snap/bin/emacs -nw --daemon
 else
     /usr/bin/emacs -nw --daemon
