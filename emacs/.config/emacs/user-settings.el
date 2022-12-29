5e35dae2bc0b889e96a231e1e46e49dd03ff1c10
Ren Odion <smakey18@gmail.com>
2022-12-29 17:21:39 -0500

    Emacs font size change

--- emacs/.config/emacs/user-settings.el
+++ emacs/.config/emacs/user-settings.el
@@ -27,7 +27,7 @@
 ;; Emoji
 (set-fontset-font t 'symbol "Noto Color Emoji")
 
-(let ((font-size 12) ;; Default font size of 13
+(let ((font-size 11)
       (height (display-pixel-height))
       (width (display-pixel-width)))
   (if (and (>= height 1440) (>= width 2560))
