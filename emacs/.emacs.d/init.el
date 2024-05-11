6eb9c823c5e1b9cd64be1adcdd01da2c217704c7
Ren Abelard Arriola Odion <smakey18@gmail.com>
2024-05-10 22:37:03 -0700

    Add nerd icons

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -522,3 +522,12 @@
   :init (doom-modeline-mode 1)
   :config
   (setq doom-modeline-vcs-max-length 30))
+
+(use-package nerd-icons
+  :ensure t
+  ;; :custom
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
+  )
