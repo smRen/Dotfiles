bf0bb88b129df54e5f4b730b1f1c65989c21cbf6
Ren Abelard Arriola Odion <smakey18@gmail.com>
2024-05-11 20:46:34 -0700

    Remove dabbrev config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -160,18 +160,6 @@
   :custom
   (json-ts-mode-indent-offset 8))
 
-;; Use Dabbrev with Corfu!
-;; (use-package dabbrev
-;;   ;; Swap M-/ and C-M-/
-;;   :bind (("M-/" . dabbrev-completion)
-;;          ("C-M-/" . dabbrev-expand))
-;;   :config
-;;   (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
-;;   ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
-;;   (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
-;;   (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
-;;   (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
-
 ;; ;; Default minibuffer completion
 ;; (use-package icomplete
 ;;   :demand t
