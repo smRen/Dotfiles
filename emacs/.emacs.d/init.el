0cf95b29088948ebc2451e59f81d9ae6d3d691e1
Ren Odion <smakey18@gmail.com>
2024-06-03 21:49:04 -0700

    Add ruby to org babel

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -162,6 +162,7 @@
         (shell . t)
         (python . t)
         (js . t)
+        (ruby . t)
         (C . t)))
   :custom
   (org-directory "~/Projects/Org")
@@ -603,5 +604,8 @@
   :straight t ; only need to install it, embark loads it after consult if found
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package inf-ruby
+  :straight t)
 
 ;;; Init.el ends here
